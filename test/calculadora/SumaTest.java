ce6780346523f5c38ee04eabe3b7d229e13a068e
JC <josecarlos.veloso468@comunidadunir.net>
2025-02-23 13:31:37 +0100

    Actualización de SumaTest.java en rama JC

--- test/calculadora/SumaTest.java
+++ test/calculadora/SumaTest.java
@@ -15,9 +15,32 @@ class SumaTest {
     })
     void sumar_dosEnteros_deberiaFuncionar(int a, int b, int resultadoEsperado) {
         // Cuando
-        int resultadoActual = Suma.sumar(a,b);
+        int resultadoActual = Suma.sumar(a, b);
         // Entonces
         assertEquals(resultadoEsperado, resultadoActual);
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "2147483647, 1, 2147483648" // Test para números grandes (límite de int)
+    })
+    void sumar_dosEnterosGrandes_deberiaFuncionar(long a, long b, long resultadoEsperado) {
+        // Cuando
+        long resultadoActual = (long) Suma.sumar((int) a, (int) b); // Convertimos a int para usar el método sumar existente
+        // Entonces
+        assertEquals(resultadoEsperado, resultadoActual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5,0,5",
+            "-3,0,-3",
+            "0,0,0"
+    })
+    void sumar_cero_deberiaFuncionar(int a, int b, int resultadoEsperado) {
+        // Cuando
+        int resultadoActual = Suma.sumar(a, b);
+        // Entonces
+        assertEquals(resultadoEsperado, resultadoActual);
+    }
 }
